24d0d43d7ff9734ef02fc9a6166811b23e2b476e
suhyeonOh <sh020119@snu.ac.kr>
2023-10-26 12:21:28 +0900

    home 화면 기본 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Thread } from './pages/Thread';
 import { Create } from './components/Create';
 import { Home } from './pages/Home';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const apiURL = import.meta.env.VITE_API_URL;
 
@@ -50,11 +51,67 @@ function Login() {
   );
 }
 function SignUp() {
+  const [username, setUsername] = useState('');
+
   const [inputId, setInputId] = useState('');
   const [inputPassword, setInputPassword] = useState('');
-  const [inputPasswordCheck, setInputPasswordCheck] = useState('');
 
-  return <></>;
+  return (
+    <>
+      <h1>username</h1>
+      <input
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      ></input>
+      <h1>id</h1>
+      <input
+        value={inputId}
+        onChange={(e) => {
+          setInputId(e.target.value);
+        }}
+      ></input>
+      <h1>password</h1>
+      <input
+        value={inputPassword}
+        onChange={(e) => {
+          setInputPassword(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={async () => {
+          setUsername('');
+          setInputId('');
+          setInputPassword('');
+          const userData = {
+            name: username,
+            id: inputId,
+            password: inputPassword,
+          };
+          try {
+            const response = await fetch(`${apiURL}/signup`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(userData),
+            });
+
+            if (response.ok) {
+              console.log('회원가입 성공');
+            } else {
+              console.error('회원가입 실패');
+            }
+          } catch (error) {
+            console.error('네트워크 오류:', error);
+          }
+        }}
+      >
+        회원가입
+      </button>
+    </>
+  );
 }
 
 function Header() {
@@ -66,20 +123,41 @@ function Header() {
 }
 function Nav() {
   return (
-    <>
-      <div style={{ height: '100px', background: 'gray' }}></div>
-    </>
+    <div className="Nav">
+      <div className="grid-container">
+        <div className="grid-item">
+          <Link style={{ color: 'black' }} to="/">
+            HOME
+          </Link>
+          <br />
+          <p>hot topic -클릭시 #이동하고, boards로 클릭하면 사라진다.</p>
+          <p>저번에 했더 velog 라우팅 기능 구현</p>
+          <p>hot topic</p>
+        </div>
+        <div className="grid-item">
+          <Link style={{ color: 'black' }} to="/">
+            BOARDS
+          </Link>
+        </div>
+        <div className="grid-item">
+          <Link style={{ color: 'black' }} to="/">
+            USERS
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
 function App() {
   return (
     <>
-      <Header></Header>
       <Nav></Nav>
+      {/*<Header></Header>*/}
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/thread/:id" element={<Thread></Thread>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
       <Create></Create>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,155 @@
+import { useEffect, useState } from 'react';
 import { ThreadsList } from '../components/ThreadsList';
+import styled, { keyframes, css } from 'styled-components';
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  width: 300px;
+  height: 300px;
+`;
+
+const Cell = styled.div`
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const NewDiv = styled.div`
+  background-color: blue;
+  width: 80%;
+  height: 80%;
+  position: absolute;
+`;
+
+interface LogoProps {
+  islogofaded: string | undefined;
+}
+interface explainProps {
+  islogofaded: string | undefined;
+}
+const textIn = keyframes`
+from {
+  opacity: 0;
+  transform: translateY(+100%);
+}
+to {
+  opacity: 1;
+  transform: translateY(0);
+}
+`;
+
+const fadeIn = keyframes`
+from {
+  opacity: 0;
+  transform: scale(0.7);
+}
+to {
+  opacity: 1;
+  transform: scale(1);
+}
+`;
+
+const TextComponent = styled.p<explainProps>`
+  opacity: 0;
+  font-size: 20px;
+  color: black;
+  animation: ${(props) =>
+    !!props.islogofaded
+      ? css`
+          ${textIn} 1s ease-in-out forwards
+        `
+      : 'none'};
+  animation-delay: 0.8s;
+`;
+
+const Mycomponent = styled.span<LogoProps>`
+  font-size: 120px;
+  color: black;
+  & > .letter {
+    display: inline-block;
+    margin-right: 0.2em;
+    opacity: 0;
+    animation: ${(props) =>
+      !!props.islogofaded
+        ? css`
+            ${fadeIn} 1s ease-in-out forwards
+          `
+        : 'none'};
+  }
+  & > .letter_T {
+    font-size: 220px;
+    animation-delay: 0.4s;
+  }
+`;
 
 //처음 접속할때 보여준다
 export function Home() {
+  const [islogofaded, setIsLogoFaded] = useState(false);
+
+  const createRandomDivs = () => {
+    const divs = [];
+    const positions = [];
+    while (divs.length < 10) {
+      const randomIndex = Math.floor(Math.random() * 25);
+      //positions에 randomIndex가 없으면
+
+      if (!positions.includes(randomIndex)) {
+        positions.push(randomIndex);
+        divs.push(
+          <NewDiv
+            key={divs.length}
+            style={
+              {
+                //   top: `${Math.floor(randomIndex / 5) * 20}%`,
+                //  left: `${(randomIndex % 5) * 20}%`,
+              }
+            }
+          />
+        );
+      }
+    }
+    return divs;
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLogoFaded(true);
+    }, 300);
+  }, []);
+  const logostr = ['G', 'E', 'T', 'W', 'O', 'R', 'L', 'D'];
+
   return (
     <>
-      <h1>home</h1>
-      <hr />
       <main>
-        <ThreadsList></ThreadsList>
-      </main>
+        <div style={{ width: '1200px' }}>
+          {' '}
+          {
+            logostr.map((str, index) => {
+              return (
+                <Mycomponent islogofaded={islogofaded ? 'true' : undefined}>
+                  <span className={`letter letter_${str}`}> {str}</span>
+                </Mycomponent>
+              );
+            })
 
-      <hr />
+            /*<ThreadsList></ThreadsList>*/
+          }
+        </div>
+        <TextComponent islogofaded={islogofaded ? 'true' : undefined}>
+          세상의 모든 이야기가 만나는 곳
+        </TextComponent>
+        <p>hot topic</p>
+
+        <Grid>
+          {Array.from({ length: 25 }, (_, index) => (
+            <Cell key={index}>{index < 10 && createRandomDivs()}</Cell>
+          ))}
+        </Grid>
+      </main>
     </>
   );
 }
